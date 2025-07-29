97e9cd3698185c61cedbbca47b4763a4d12ce163
roddazad <roddazad@gmail.com>
2025-07-28 19:30:13 -0700

    Initial commit:  setup the app and created the Home page header

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import "../styles/Home.css"
+
+const Home = () => {
+    return ( 
+        <div className="container">
+            <h1>Memory Game</h1>
+            <button className="button"> New Game</button>
+        </div>
+     );
+}
+ 
+export default Home;
